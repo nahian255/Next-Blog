dc2f84aefafd3756e51ee1248c0a5ba8fe5ccc82
nahian255 <nahimn253@gmail.com>
2023-12-18 09:13:50 +0600

    new start

--- src/lib/model/product.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"
-
-const productModel = new mongoose.Schema({
-    name: String,
-    url: String
-});
-
-export const product = mongoose.models.item || mongoose.model('item', productModel)
